9b2badc1e2c1b59a2f5378890620ac2cada32022
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-05-25 22:53:16 -0300

    adiciona bibliotecas do projeto

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-
-int main()
-{
+int main() {
     stdio_init_all();
 
     while (true) {
